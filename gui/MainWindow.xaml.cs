a0cdea3688b702f9346e99a24f9e73f1a89ae3d5
William Bons <laurent@liberatum.ru>
2025-07-19 13:36:39 +0300

    beginning

--- gui/MainWindow.xaml.cs
+++ gui/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace Castor
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow()  
         {
             InitializeComponent();
             Console.Print("wellcommen..+");
